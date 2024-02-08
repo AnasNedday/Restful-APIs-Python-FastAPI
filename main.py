914e5d7537bf0d003947cfa481b07baffd2b24cc
Anas Nedday <97068637+AnasNedday@users.noreply.github.com>
2024-02-08 19:40:42 +0100

    POST GET METHODS

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI 
-
+from models import User, Gender, Role
+from typing import List
+from uuid import uuid4
 
 app = FastAPI()
 
+db: List[User] = [
+    User(id=uuid4(),
+    first_name="Ibtissam",
+    last_name="Mekkaoui",
+    middle_name="",
+    gender=Gender.female,
+    roles=[Role.student]
+    ),
+    User(id=uuid4(),
+    first_name="Anas",
+    last_name="Nedday",
+    middle_name="",
+    gender=Gender.male,
+    roles=[Role.admin]
+    )
+
+]
+
+
 @app.get("/")
-def root():
+async def root():
     return {"Hello": "Mundo"}
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
